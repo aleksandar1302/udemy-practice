5c0a89e3b3078efa823663ce0e6f13ce63fcc088
aleksandar1302 <salejankovic95@gmail.com>
2018-12-25 23:04:25 +0100

    Updated ip address and port

--- Express/FirstExpressApp/app.js
+++ Express/FirstExpressApp/app.js
@@ -26,6 +26,6 @@ app.get("*", function(req,res){
     res.send("You are a star!");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, '127.0.0.1', function(){
     console.log("Server has started");
 });
